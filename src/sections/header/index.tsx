69620fbb37dc3661e2ae009992d8a05bda9326ab
MosabShaheen <99540632+MosabShaheen@users.noreply.github.com>
2023-08-25 13:04:19 +0500

    Fineshed

--- src/sections/header/index.tsx
+++ src/sections/header/index.tsx
@@ -1,81 +1,84 @@
 "use client";
+
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
-  const [tab, activeTab] = useState(true);
+  const [tab, setTab] = useState(true);
   const [cartValue, setCartValue] = useState(0);
 
   useEffect(() => {
-    const getData = async () => {
+    const fetchCartData = async () => {
+      try {
         const response = await fetch("/api/cart", {
-            method: "GET",
-            cache: "no-cache"
-        })
-        const res = await response.json()
-        setCartValue(res.quantity)
-    }
-    getData();
+          method: "GET",
+          cache: "no-cache"
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setCartValue(data.quantity);
+        }
+      } catch (error) {
+        console.error("Error fetching cart data:", error);
+      }
+    };
+
+    fetchCartData();
   }, [cartValue]);
+
   return (
     <header className="container mx-auto">
       <nav className="flex mx-auto justify-between items-center py-4 sm:px-10">
         <div className="py-10">
           <Link href="/">
-            <Image src="/Logo.png" alt="Logo" width="158" height="27" />
+            <Image src="/Logo.png" alt="Logo" width={158} height={27} />
           </Link>
         </div>
 
-        <Link
-          className="font-sora text-base tracking-wide hidden lg:block"
-          href="/female"
-        >
-          Female
-        </Link>
-        <Link
-          className="font-sora text-base tracking-wide hidden lg:block"
-          href="/male"
-        >
-          Male
-        </Link>
-        <Link
-          className="font-sora text-base tracking-wide hidden lg:block"
-          href="/kids"
-        >
-          Kids
-        </Link>
-        <Link
-          className="font-sora text-base tracking-wide hidden lg:block"
-          href="/all-products"
-        >
-          All Product
-        </Link>
+        {/* Desktop navigation links */}
+        <div className="hidden lg:flex gap-20">
+          <Link className="font-sora text-base tracking-wide" href="/female">
+            Female
+          </Link>
+          <Link className="font-sora text-base tracking-wide" href="/male">
+            Male
+          </Link>
+          <Link className="font-sora text-base tracking-wide" href="/kids">
+            Kids
+          </Link>
+          <Link className="font-sora text-base tracking-wide" href="/all-products">
+            All Products
+          </Link>
+        </div>
+
+        {/* Mobile navigation toggle */}
         <div className="-mr-1 lg:hidden">
           <button
-            className="relative z-10 flex h-8 w-8 items-center justify-center [&amp;:not(:focus-visible)]:focus:outline-none"
+            className="relative z-10 flex h-8 w-8 items-center justify-center focus:outline-none"
             aria-label="Toggle Navigation"
             type="button"
-            onClick={() => activeTab(!tab)}
+            onClick={() => setTab(!tab)}
           >
             {tab ? (
               <svg
-                stroke="currentColor"
-                fill="currentColor"
-                stroke-width="0"
-                viewBox="0 0 20 20"
-                aria-hidden="true"
-                className="text-3xl cursor-pointer"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 20 20"
+              aria-hidden="true"
+              className="text-3xl cursor-pointer"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
             ) : (
               <svg
                 stroke="currentColor"
@@ -91,78 +94,82 @@ export default function Header() {
               </svg>
             )}
           </button>
-          {tab === false ? (
+          
+          {/* Mobile navigation menu */}
+          {tab === false && (
             <div className="fixed h-screen bg-white w-screen left-0 bottom-0 top-20 z-50">
               <div className="flex items-center justify-center h-full">
                 <div className="flex flex-col items-center h-[40%] justify-between">
                   <Link
                     className="font-sora text-lg tracking-wide"
                     href="/female"
-                    onClick={() => activeTab(!tab)}
+                    onClick={() => setTab(true)}
                   >
                     Female
                   </Link>
                   <Link
                     className="font-sora text-lg tracking-wide"
                     href="/male"
-                    onClick={() => activeTab(!tab)}
+                    onClick={() => setTab(true)}
                   >
                     Male
                   </Link>
                   <Link
                     className="font-sora text-lg tracking-wide"
                     href="/kids"
-                    onClick={() => activeTab(!tab)}
+                    onClick={() => setTab(true)}
                   >
                     Kids
                   </Link>
                   <Link
                     className="font-sora text-lg tracking-wide"
                     href="/all-products"
-                    onClick={() => activeTab(!tab)}
+                    onClick={() => setTab(true)}
                   >
                     All Products
                   </Link>
                   <Link
                     className="font-sora text-lg tracking-wide rounded-full bg-[#F1F1F1] p-3 relative"
                     href="/cart"
-                    onClick={() => activeTab(!tab)}
+                    onClick={() => setTab(true)}
                   >
                     <svg
-                      stroke="currentColor"
-                      fill="none"
-                      stroke-width="0"
-                      viewBox="0 0 24 24"
-                      height="22"
-                      width="22"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
-                        fill="currentColor"
-                      ></path>
-                      <path
-                        d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
-                        fill="currentColor"
-                      ></path>
-                      <path
-                        d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
-                        fill="currentColor"
-                      ></path>
-                    </svg>
-                    <p className="absolute top-0 right-2 text-xs px-1 bg-red-600 rounded-full text-white">{cartValue}</p>
+              stroke="currentColor"
+              fill="none"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              height="22"
+              width="22"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
+                fill="currentColor"
+              ></path>
+              <path
+                d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
+                fill="currentColor"
+              ></path>
+              <path
+                d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+                    <p className="absolute top-0 right-2 text-xs px-1 bg-red-600 rounded-full text-white">
+                      {cartValue}
+                    </p>
                   </Link>
                 </div>
               </div>
             </div>
-          ) : (
-            <div></div>
           )}
         </div>
+
+        {/* Desktop search bar */}
         <div className="items-center border border-gray-300 rounded-md gap-1 px-2 py-1 hidden lg:flex">
-          <svg
+        <svg
             stroke="currentColor"
             fill="currentColor"
             stroke-width="0"
@@ -191,9 +198,10 @@ export default function Header() {
           />
         </div>
 
-        <div className=" items-center gap-3 hidden lg:flex">
-          <Link href="/cart" className="rounded-full bg-[#F1F1F1] p-3 relative">
-            <svg
+        {/* Desktop cart icon */}
+        <div className="items-center gap-3 hidden lg:flex">
+          <Link className="rounded-full bg-[#F1F1F1] p-3 relative" href="/cart">
+          <svg
               stroke="currentColor"
               fill="none"
               stroke-width="0"
@@ -217,9 +225,7 @@ export default function Header() {
                 fill="currentColor"
               ></path>
             </svg>
-            <span
-              className={`absolute top-0 right-2 text-xs px-1 bg-red-600 rounded-full text-white`}
-            >
+            <span className={`absolute top-0 right-2 text-xs px-1 bg-red-600 rounded-full text-white`}>
               {cartValue}
             </span>
           </Link>
